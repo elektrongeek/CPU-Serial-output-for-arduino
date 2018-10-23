fcbc74081384434c8683f0219eedebf7b049a3c2
Jikhai <jikhai@Marisa-K.mshome>
2018-10-23 10:47:05 +0200

    code modified to cover more usefull info : Ram, uptime…

--- Sources/Test serial/Form1.Designer.cs
+++ Sources/Test serial/Form1.Designer.cs
@@ -33,6 +33,13 @@
             this.Clock = new System.Windows.Forms.Timer(this.components);
             this.Start = new System.Windows.Forms.Button();
             this.Output = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Cpu_USAGE
@@ -40,17 +47,18 @@
             this.Cpu_USAGE.AutoSize = true;
             this.Cpu_USAGE.Location = new System.Drawing.Point(12, 9);
             this.Cpu_USAGE.Name = "Cpu_USAGE";
-            this.Cpu_USAGE.Size = new System.Drawing.Size(0, 13);
+            this.Cpu_USAGE.Size = new System.Drawing.Size(29, 13);
             this.Cpu_USAGE.TabIndex = 2;
+            this.Cpu_USAGE.Text = "CPU";
             // 
             // Clock
             // 
-            this.Clock.Interval = 500;
-            this.Clock.Tick += new System.EventHandler(this.timer1_Tick);
+            this.Clock.Interval = 1000;
+            this.Clock.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Start
             // 
-            this.Start.Location = new System.Drawing.Point(12, 40);
+            this.Start.Location = new System.Drawing.Point(223, 147);
             this.Start.Name = "Start";
             this.Start.Size = new System.Drawing.Size(75, 23);
             this.Start.TabIndex = 3;
@@ -61,18 +69,82 @@
             // Output
             // 
             this.Output.AutoSize = true;
-            this.Output.Location = new System.Drawing.Point(113, 44);
+            this.Output.Location = new System.Drawing.Point(332, 151);
             this.Output.Name = "Output";
             this.Output.Size = new System.Drawing.Size(79, 17);
             this.Output.TabIndex = 4;
             this.Output.Text = "Serial out ?";
             this.Output.UseVisualStyleBackColor = true;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "NetBios";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "OSVER";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(113, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "UPTIME";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "RAM";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(220, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "USER";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(218, 75);
+            this.ClientSize = new System.Drawing.Size(423, 180);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Output);
             this.Controls.Add(this.Start);
             this.Controls.Add(this.Cpu_USAGE);
@@ -88,6 +160,13 @@
         private System.Windows.Forms.Timer Clock;
         private System.Windows.Forms.Button Start;
         private System.Windows.Forms.CheckBox Output;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Sources/Test serial/Form1.cs
+++ Sources/Test serial/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/* I stole this code from ElektronGeek, but i love him so it doesn't matter, also i made some improvements */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Windows.Forms;
 using System.IO;
 using  System.IO.Ports;
 using System.Diagnostics;
+using System.Management;
 using System.Timers;
 
 namespace Test_serial
@@ -19,24 +21,23 @@ namespace Test_serial
     {
 
         // Creating and opening a serial port (here it is COM4)
-        SerialPort Portserie = new SerialPort("COM4", 9800, Parity.None, 8, StopBits.One);
+        SerialPort Portserie = new SerialPort("COM3", 9800, Parity.None, 8, StopBits.One);
 
 
         // Creating CPU Counter to read CPU % Usage
         private PerformanceCounter CPUCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        
 
     
         public Form1()
         {
             InitializeComponent();
-           
-            
         }
 
 
         private void Start_Click(object sender, EventArgs e)
         {
-            Clock.Enabled = true;
+              Clock.Enabled = true;
             if (Output.Checked == true)
             {
                 //Opening serial port
@@ -46,22 +47,60 @@ namespace Test_serial
         }
 
         // Using a timer to send data (configurated in the design view (default value 500ms)
-        private void timer1_Tick(object sender, EventArgs e)
+        private void Timer1_Tick(object sender, EventArgs e)
         {
-            //Creating new string to hold data
-            String CPU;
-            
-            //Filling CPU String with DATA
-            CPU = CPUCounter.NextValue().ToString();
+            long MaxRam = 4294967296 * 2;
 
-            //Writing CPU usage into programm window
-            Cpu_USAGE.Text = "CPU Usage " + CPU as string;
+
+            String CPU = "CPU : " + Math.Round(CPUCounter.NextValue(), 1).ToString() + "%";
+                Process[] processes = Process.GetProcesses();
+                long UsedRam = 0;
+                foreach (Process process in processes)
+                {
+                UsedRam += process.WorkingSet64;
+                }
+            String RAM = ("RAM : " + ((UsedRam*100)/MaxRam).ToString() + "%");
+            String NetBios = Environment.MachineName;
+            String NTver  = Environment.OSVersion.ToString();
+                if (Equals(NTver.Substring(0, 9),"Microsoft")) NTver = NTver.Substring(18, NTver.Length-18);
+            String OS;
+                var name = (from x in new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem").Get().Cast<ManagementObject>()
+                select x.GetPropertyValue("Caption")).FirstOrDefault();
+                OS = name != null ? name.ToString() : "Unknown";
+                if (Equals(OS.Substring(0, 9), "Microsoft")) OS = OS.Substring(10, OS.Length - 10);
+            String User = Environment.UserName;
+            String UpTime = "Uptime : " + TimeSpan.FromMilliseconds(Environment.TickCount).ToString(@"dd\:hh\:mm\:ss");
+
+           
+            Cpu_USAGE.Text = CPU;
+            label2.Text = OS;
+            label1.Text = NetBios;
+            label3.Text = NTver;
+            label4.Text = UpTime;
+            label5.Text = RAM;
+            label6.Text = User;
+            //Sending data over the port
             if (Output.Checked == true)
             {
                 if (Portserie.IsOpen == true)
                 {
+                    //for now i wait bewteen every information since i use a small lcd that can only display a few characters at a time
                     //Writing CPU String over serial 
-                    Portserie.Write(CPU as string);
+                    Portserie.Write(CPU);
+                    System.Threading.Thread.Sleep(2000);
+                    Portserie.Write(User + "@" + NetBios);
+                    System.Threading.Thread.Sleep(2000);
+                    Portserie.Write(RAM);
+                    System.Threading.Thread.Sleep(2000);
+                    Portserie.Write(OS);
+                    System.Threading.Thread.Sleep(2000);
+                    Portserie.Write(NTver);
+                    System.Threading.Thread.Sleep(2000);
+                    Portserie.Write(UpTime);
+                    System.Threading.Thread.Sleep(2000);
+                    
+
+
                 }
                 else
                 {
@@ -70,7 +109,5 @@ namespace Test_serial
                 }
             }
         }
-
-       
     }
 }
